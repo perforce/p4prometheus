cd764dc05555cc0981e8c3512b25fb3eedd1fa2f
Robert Cowham <rcowham@perforce.com>
2020-01-07 14:34:50 +0000

    Refactor test to ensure timing issues not a problem

--- p4prom_test.go
+++ p4prom_test.go
@@ -52,12 +52,14 @@ func (t *mockTailer) addLines(lines []string) {
 
 func getOutput(testchan chan string) []string {
 	result := make([]string, 0)
-	eol := regexp.MustCompile("\r\n|\n")
+	lastoutput := ""
 	for output := range testchan {
-		for _, line := range eol.Split(output, -1) {
-			if len(line) > 0 && !strings.HasPrefix(line, "#") {
-				result = append(result, line)
-			}
+		lastoutput = output
+	}
+	eol := regexp.MustCompile("\r\n|\n")
+	for _, line := range eol.Split(lastoutput, -1) {
+		if len(line) > 0 && !strings.HasPrefix(line, "#") {
+			result = append(result, line)
 		}
 	}
 	return result
@@ -80,35 +82,22 @@ func TestP4Prom(t *testing.T) {
 	p4p.logger = logger
 	done := make(chan int, 1)
 	go func() {
-		logger.Infof("Starting to process events")
+		logger.Debugf("Starting to process events")
 		result := p4p.ProcessEvents(10*time.Millisecond, tailer, done)
-		logger.Infof("Finished process events")
+		logger.Debugf("Finished process events")
 		assert.Equal(t, 0, result)
 	}()
 	time.Sleep(20 * time.Millisecond)
-	logger.Infof("Sending done")
+	logger.Debugf("Sending done")
 	done <- 1
-	logger.Infof("Getting output")
+	logger.Debugf("Getting output")
 	lines := getOutput(testchan)
-	logger.Infof("Got output")
+	logger.Debugf("Got output")
 	assert.Equal(t, 3, len(lines))
 	eol := regexp.MustCompile("\r\n|\n")
 	expected := eol.Split(`p4_prom_log_lines_read{serverid=""} 0
 p4_prom_cmds_processed{serverid=""} 0
 p4_prom_cmds_pending{serverid=""} 0`, -1)
 	assert.Equal(t, expected, lines)
-
-	// 	testInput := `
-	// Perforce server info:
-	// 	2015/09/02 15:23:09 pid 1616 robert@robert-test 127.0.0.1 [Microsoft Visual Studio 2013/12.0.21005.1] 'user-sync //...'
-	// Perforce server info:
-	// 	2015/09/02 15:23:09 pid 1616 compute end .031s
-	// Perforce server info:
-	// 	2015/09/02 15:23:09 pid 1616 completed .031s`
-
-	// go fp.P4LogParseFile(*opts, outchan)
-	// output := getResult(outchan)
-	// assert.Equal(t, `{"processKey":"4d4e5096f7b732e4ce95230ef085bf51","cmd":"user-sync","pid":1616,"lineNo":2,"user":"robert","workspace":"robert-test","computeLapse":0.031,"completedLapse":0.031,"ip":"127.0.0.1","app":"Microsoft Visual Studio 2013/12.0.21005.1","args":"//...","startTime":"2015/09/02 15:23:09","endTime":"2015/09/02 15:23:09","running":0,"uCpu":0,"sCpu":0,"diskIn":0,"diskOut":0,"ipcIn":0,"ipcOut":0,"maxRss":0,"pageFaults":0,"rpcMsgsIn":0,"rpcMsgsOut":0,"rpcSizeIn":0,"rpcSizeOut":0,"rpcHimarkFwd":0,"rpcHimarkRev":0,"rpcSnd":0,"rpcRcv":0,"cmdError":false,"tables":[]}`,
-	// 	output[0])
 
 }
