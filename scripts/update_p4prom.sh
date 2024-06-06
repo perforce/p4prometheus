812fdc49883def0320a513d38064bfd355b3dec2
Robert Cowham <rcowham@perforce.com>
2024-06-06 15:15:21 +0100

    Install new version v0.8.5 of p4prometheus

--- scripts/update_p4prom.sh
+++ scripts/update_p4prom.sh
@@ -18,7 +18,7 @@ metrics_root=/hxlogs/metrics
 metrics_link=/p4/metrics
 
 VER_NODE_EXPORTER="1.3.1"
-VER_P4PROMETHEUS="0.8.3"
+VER_P4PROMETHEUS="0.8.5"
 
 # Default to amd but allow arm architecture
 arch="amd64"
