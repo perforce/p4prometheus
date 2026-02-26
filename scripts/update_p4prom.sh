f5c31c4d2b52cde3e8887d93f10e87695d6535e7
Robert Cowham <rcowham@perforce.com>
2026-02-26 18:02:17 +0000

    Install/update v0.10.5

--- scripts/update_p4prom.sh
+++ scripts/update_p4prom.sh
@@ -22,7 +22,7 @@ vmagent_config_dir="/var/vmagent"
 
 
 VER_NODE_EXPORTER="1.3.1"
-VER_P4PROMETHEUS="0.10.4"
+VER_P4PROMETHEUS="0.10.5"
 VER_VICTORIA_METRICS="1.131.0"
 
 # Default to amd but allow arm architecture
