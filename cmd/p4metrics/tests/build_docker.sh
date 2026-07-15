35726ec8b551362643d98c0cb66d7b57c9b63c90
Robert Cowham <rcowham@perforce.com>
2026-07-15 15:19:07 +0100

    More tidying up of tests

--- cmd/p4metrics/tests/build_docker.sh
+++ cmd/p4metrics/tests/build_docker.sh
@@ -3,15 +3,15 @@
 set -u
 
 #------------------------------------------------------------------------------
-# Build the Docker containers for p4metrics testing - but using podman for systemd support
+# Build the Docker containers for p4metrics/p4prometheus scripts testing - but using podman for systemd support
 
 # Usage Examples:
 #    build_docker.sh
 #
-# Goes together with run_docker_tests.sh
+# Goes together with run_docker_tests.sh in the same directory, which runs the tests in the built containers.
 # This is provided as a useful tool for testing!
 
-# We calculate dir relative to directory of script
+# We calculate dir relative to directory of script - the root of p4prometheus project.
 script_dir="${0%/*}"
 root_dir="$(cd "$script_dir/../../.."; pwd -P)"
 
@@ -29,6 +29,8 @@ for image in sdp; do
     cname=p4metricstest
     podman run --cap-add=SYS_RESOURCE,AUDIT_WRITE -d --rm -v $root_dir/cmd/p4metrics:/p4metrics --name $cname perforce/p4metricstest-${image}
     podman exec -it $cname bash -xv /p4metrics/docker/docker_setup_p4metrics_tests.sh
+    # Having executed the setup script, we commit the container to a new image so that it can be used for testing without 
+    # having to re-run the setup script each time.
     podman commit $cname base_$cname
     podman kill $cname
 done
